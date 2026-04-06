717f7eb3063f6dbb65f7073a391c21cbdfe70aed
Artem Petrov <artempetrovjob@mail.ru>
2026-04-06 21:49:17 +0300

    chore: configure github pages and minor project fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend-challenge/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
